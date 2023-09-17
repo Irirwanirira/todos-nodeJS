1b1d23b106e1a531b46120a1415521b1567edebb
Irirwanirira <irijoseph41@gmail.com>
2023-09-17 11:49:58 +0200

    Add test coverage

--- test/todos.test.js
+++ test/todos.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 require('dotenv').config();
 const app = require('../src/index');
+const { softDelete, editTodo } = require('../src/controllers')
 const todos = require('../src/db');
 
 
@@ -25,23 +26,111 @@ describe('GET /api/listTodo', () => {
   });
 });
 
-// describe('POST /api/postTodo', () => {
-//   it('should create a new todo', async () => {
-//     const newTodo = {
-//       id: arr.length + 1,
-//       task: 'coding',
-//       completed: false,
-//     };
+describe('POST /api/postTodo', () => {
+  it('should create a new todo', async () => {
+    let date = new Date();
+    date = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+    const newTodo = {
+      id:4,
+      task: "Eat",
+      completed:false,
+      date,
+      isDeleted: false
+    }
+    todos.push(newTodo)
 
-//     todos.push(newTodo);
-//     const res = await request(app).post('/api/postTodo').send(newTodo);
-//     expect(res.statusCode).toBe(201);
-//   });
-// });
-
-describe('DELETE /deleteTodo/:id', () => {
-  it('should delete a todo', async () => {
-    const res = await request(app).delete('/deleteTodo/4');
-    expect(res.statusCode).toBe(200);
+    const res = await request(app).post('/api/postTodo').send(newTodo);
+    expect(res.statusCode).toBe(201);
+    expect(newTodo.task).toBe("Eat");
+    expect(typeof newTodo.isDeleted).toBe("boolean");
   });
 });
+
+describe('GET /api/listTodo/id', () => {
+  it('should return all the todo', async () => {
+    const id = 1; 
+    const res = await request(app).get(`/api/listTodo/${id}`);
+    
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(todos)).toBe(true);
+    todos.forEach(todo => {
+      expect(typeof todo.completed).toBe("boolean");
+      expect(typeof todo.task).toBe("string");
+      expect(todo).toHaveProperty("task");
+      if (todo.id === id) {
+        expect(todo).toHaveProperty("id", id);
+      }
+    });
+  });
+});
+
+describe('softDelete', () => {
+  it('should soft delete a todo with the given id', () => {
+    const req = { params: { id: 1 } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const expectedResponse = { message: `Todo 1 has been softly deleted` };
+
+    softDelete(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(expectedResponse);
+    expect(todos.some(todo => todo.id === 1 && todo.isDeleted)).toBe(true);
+  });
+
+  it('should return an error if the todo does not exist', () => {
+    const req = { params: { id: 1000 } }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const expectedResponse = { message: `Unable to Remove 1000 from the list` };
+
+    softDelete(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(expectedResponse);
+  });
+});
+
+describe('editTodo', () => {
+  it('should edit a todo with the given id', () => {
+    const req = { params: { id: 1 }, body: { task: 'Updated task' } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    editTodo(req, res);
+    expect(todos.some(todo => todo.id === req.params.id && todo.task === 'Updated task')).toBe(false);
+  });
+
+  it('should return an error if the todo does not exist', () => {
+    const req = { params: { id: 999 }, body: { task: 'Updated task' } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const expectedResponse = { message: `Todo 999 doesn't exist` };
+
+    editTodo(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith(expectedResponse);
+  });
+
+  it('should return an error if the task is not provided', () => {
+    const req = { params: { id: 1 }, body: {} };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    editTodo(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+  });
+});
+
+
+
